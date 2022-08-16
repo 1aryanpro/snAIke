f1206c27af9d272f46bc9d7937719338cc209108
Aryan Prodduturi <32280917+SmashMineGame@users.noreply.github.com>
2022-08-16 17:37:45 -0400

    finished movement code

--- sketch.js
+++ sketch.js
@@ -1,20 +1,39 @@
 let [WIDTH, HEIGHT] = [20, 20];
 let s = 30;
-let apples = [];
-let snake = [[10, 10]];
+let apple;
+let snake = [
+  [10, 10],
+  [11, 10],
+  [12, 10],
+  [13, 10],
+];
+let dir = 0;
+let ndir = -1;
+
+let timer = 1000;
 
 function setup() {
   createCanvas(WIDTH * s, HEIGHT * s, P2D);
-  newApple();
   newApple();
 }
 
 function draw() {
   background(51);
 
+  timer -= deltaTime;
+  if (timer <= 0) {
+    move(ndir == -1 ? dir : ndir);
+    timer = 150;
+
+    if (ndir != -1) {
+      dir = ndir;
+      ndir = -1;
+    }
+  }
+
   for (let x = 0; x < WIDTH; x++) {
     for (let y = 0; y < HEIGHT; y++) {
-      if (checkArr(apples, x, y)) fill(255, 60, 60);
+      if (checkArr([apple], x, y)) fill(255, 60, 60);
       else if (checkArr(snake, x, y)) fill(60, 200, 60);
       else noFill();
 
@@ -24,7 +43,11 @@ function draw() {
 }
 
 function newApple() {
-  apples.push([floor(random(WIDTH)), floor(random(HEIGHT))]);
+  let ap;
+  do {
+    ap = [floor(random(WIDTH)), floor(random(HEIGHT))];
+  } while (checkArr(snake, ...ap));
+  apple = ap;
 }
 
 function checkArr(arr, x, y) {
@@ -35,3 +58,45 @@ function checkArr(arr, x, y) {
   return check;
 }
 
+function move(dir) {
+  //   1
+  // 0 + 2
+  //   3
+
+  head = [...snake[0]];
+  switch (dir) {
+    case 0:
+      head[0]--;
+      break;
+    case 1:
+      head[1]--;
+      break;
+    case 2:
+      head[0]++;
+      break;
+    case 3:
+      head[1]++;
+      break;
+  }
+
+  if (checkArr(snake, ...head)) {
+    return;
+    // switch to game over condition
+  }
+
+  if (head[0] < 0 || head[0] >= WIDTH || head[1] < 0 || head[1] >= HEIGHT)
+    return;
+
+  if (checkArr([apple], ...head)) {
+    newApple();
+  } else snake.pop();
+  snake.unshift(head);
+}
+
+function keyPressed() {
+  if (keyCode > 36 && keyCode < 41 && ndir == -1) {
+    let keyDir = keyCode - 37;
+    // if ((dir - keyDir) % 2 == 0) return;
+    ndir = keyDir;
+  }
+}
